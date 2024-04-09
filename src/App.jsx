2aa88c8d9c762d8b7c5ba92c70a4a98477a27686
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2024-04-09 10:48:14 +0100

    readme edited

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src="/tm-logo.png" alt="Tosin Media" />
+        <h1>Tosin Media Official Website</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
